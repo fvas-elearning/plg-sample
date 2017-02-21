5176bb093f2e6a28936deeb31b39ece82ddff9cf
tropotek <info@tropotek.com>
2017-02-22 08:20:40 +1100

    Finished tiding up user areas

--- config.php
+++ config.php
@@ -10,14 +10,14 @@ $config = \Tk\Config::getInstance();
 $routes = $config['site.routes'];
 
 $params = array('access' => \App\Db\User::ROLE_ADMIN);
-$routes->add('sample-admin-settings', new \Tk\Routing\Route('/sample/adminSettings.html', 'Ems\Controller\SystemSettings::doDefault', $params));
+$routes->add('Sample Admin Settings', new \Tk\Routing\Route('/sample/adminSettings.html', 'Ems\Controller\SystemSettings::doDefault', $params));
 
 
 $params = array('access' => \App\Db\User::ROLE_CLIENT);
-$routes->add('sample-sample-institution-settings', new \Tk\Routing\Route('/sample/institutionSettings.html', 'Ems\Controller\InstitutionSettings::doDefault', $params));
+$routes->add('Sample Institution Settings', new \Tk\Routing\Route('/sample/institutionSettings.html', 'Ems\Controller\InstitutionSettings::doDefault', $params));
 
 
 $params = array('access' => array(\App\Db\User::ROLE_CLIENT, \App\Db\User::ROLE_STAFF));
-$routes->add('sample-sample-course-settings', new \Tk\Routing\Route('/sample/courseSettings.html', 'Ems\Controller\CourseSettings::doDefault', $params));
+$routes->add('Sample Course Settings', new \Tk\Routing\Route('/sample/courseSettings.html', 'Ems\Controller\CourseSettings::doDefault', $params));
 
 

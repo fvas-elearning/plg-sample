923d3ee870929474523bc330f80ffb953e75b733
tropotek <info@tropotek.com>
2017-06-20 13:54:34 +1000

    Minor Code Updates - 252s-dev.vet.unimelb.edu.au

--- config.php
+++ config.php
@@ -9,16 +9,16 @@ $config = \Tk\Config::getInstance();
 /** @var \Tk\Routing\RouteCollection $routes */
 $routes = $config['site.routes'];
 
-$params = array('role' => \App\Db\UserGroup::ROLE_ADMIN);
+$params = array('role' => 'admin');
 $routes->add('Sample Admin Settings', new \Tk\Routing\Route('/sample/adminSettings.html', 'Ems\Controller\SystemSettings::doDefault', $params));
 
-$params = array('role' => \App\Db\UserGroup::ROLE_CLIENT);
+$params = array('role' => 'client');
 $routes->add('Sample Institution Settings', new \Tk\Routing\Route('/sample/institutionSettings.html', 'Ems\Controller\InstitutionSettings::doDefault', $params));
 
-$params = array('role' => array(\App\Db\UserGroup::ROLE_CLIENT, \App\Db\UserGroup::ROLE_STAFF));
+$params = array('role' => array('client', 'staff'));
 $routes->add('Sample Course Profile Settings', new \Tk\Routing\Route('/sample/courseProfileSettings.html', 'Ems\Controller\CourseProfileSettings::doDefault', $params));
 
-$params = array('role' => array(\App\Db\UserGroup::ROLE_CLIENT, \App\Db\UserGroup::ROLE_STAFF));
+$params = array('role' => array('client', 'staff'));
 $routes->add('Sample Course Settings', new \Tk\Routing\Route('/sample/courseSettings.html', 'Ems\Controller\CourseSettings::doDefault', $params));
 
 
